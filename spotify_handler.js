e76dac158ed1984100522f8f1de3878b9d5696bf
daniel <danielh714@gmail.com>
2017-12-09 19:11:27 -0700

    break into js

---
+++ spotify_handler.js
@@ -0,0 +1,31 @@
+var hash = window.location.hash.substr(1);
+
+if (hash == "") {
+  window.location = "https://accounts.spotify.com/authorize?redirect_uri=https://danielkyh.github.io/testspotauth/index.html&client_id=33c8e9e18c914c99a33a2ef7ff6d1a60&response_type=token"
+}
+
+var hashParams = hash.split('&').reduce(function (result, item) {
+  var parts = item.split('=');
+  result[parts[0]] = parts[1];
+  return result;
+}, {});
+
+console.log(hashParams);
+
+$(document).ready(function() {
+  $('#search-button').on("click", function() {
+    let albumName = $('#search').val()
+    let queryURL = "https://api.spotify.com/v1/search?type=album&market=US&limit=5&q=" + albumName
+    $.ajax({
+      type: "GET",
+      url: queryURL,
+      dataType: 'json'
+    }).done(function(response) {
+      console.log(response)
+    })
+  })
+})
+
+
+
+
